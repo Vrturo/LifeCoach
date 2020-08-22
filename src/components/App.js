65c6dcab4e9c20bdca473f66366ca3b1c66d24f6
Perez <perezart@amazon.com>
2020-08-22 16:45:27 -0700

    begin trimming html onto React app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header.js'
+import Top from './Top.js'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Top />
     </div>
   );
 }
